ea4f4e570a3d4a15712ec1cb9baa2e49bc35b9fa
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-24 01:36:52 +0600

    added singly linked list class

---
+++ src/DataStructures/LinkedList/Singly.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sajibsrs\DataStructures\LinkedList;
+
+/**
+ * Singly linked list.
+ */
+class Singly
+{
+    public SinglyNode $firstNode;
+
+    public function __construct(string $data)
+    {
+        $this->firstNode = new SinglyNode($data);
+    }
+
+    /**
+     * Insert at the beginning.
+     */
+    public function insert(string $data): bool
+    {
+        $newNode = new SinglyNode($data);
+        $newNode->nextNode = $this->firstNode;
+        $this->firstNode = $newNode;
+
+        return true;
+    }
+
+    /**
+     * Add to a specific position of the list.
+     */
+    public function add(int $index, string $data): bool
+    {
+        $newNode = new SinglyNode($data);
+
+        if ($index === 0) {
+            $newNode->nextNode = $this->firstNode;
+            $this->firstNode = $newNode;
+            return true;
+        }
+
+        $currentNode = $this->firstNode;
+        $currentIndex = 0;
+
+        while ($currentIndex < $index - 1 && $currentNode !== null) {
+            $currentNode = $currentNode->nextNode;
+            $currentIndex++;
+        }
+        $newNode->nextNode = $currentNode->nextNode;
+        $currentNode->nextNode = $newNode;
+
+        return true;
+    }
+
+    /**
+     * Read a specific item from the list.
+     */
+    public function read(int $index)
+    {
+        $currentNode = $this->firstNode;
+        $currentIndex = 0;
+
+        while ($currentIndex < $index) {
+            $currentNode = $currentNode->nextNode;
+
+            if ($currentNode === null) {
+                return false;
+            }
+
+            $currentIndex++;
+        }
+        return $currentNode;
+    }
+
+    /**
+     * Search for item. Returns found item or false on failure.
+     */
+    public function search(string $data)
+    {
+        $currentNode = $this->firstNode;
+        $currentIndex = 0;
+
+        while ($currentNode) {
+            if ($currentNode->data === $data) {
+                return $currentIndex;
+            }
+            $currentNode = $currentNode->nextNode;
+            $currentIndex++;
+        }
+
+        return false;
+    }
+
+    /**
+     * Delete list node by index.
+     */
+    public function delete(int $index)
+    {
+        if ($index === 0) {
+            $this->firstNode = $this->firstNode->nextNode;
+            return;
+        }
+        $currentNode = $this->firstNode;
+        $currentIndex = 0;
+
+        while ($currentIndex < $index - 1 && $currentNode !== null) {
+            $currentNode = $currentNode->nextNode;
+            $currentIndex++;
+        }
+        $nodeAfterDeleted = $currentNode->nextNode->nextNode;
+        $currentNode->nextNode = $nodeAfterDeleted;
+    }
+    
+    /**
+     * Echo the items of the list.
+     */
+    public function display(): void
+    {
+        $currentNode = $this->firstNode;
+
+        while ($currentNode !== null) {
+            echo $currentNode->data;
+            $currentNode = $currentNode->nextNode;
+        }
+    }
+}
