0bd6af5e9e8c657ff9ee59a7feef24812064c844
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-26 05:13:39 +0600

    modified to be compatible with queue

--- src/DataStructure/LinkedList/Doubly.php
+++ src/DataStructure/LinkedList/Doubly.php
@@ -9,27 +9,38 @@ class Doubly
     public ?DoublyNode $firstNode = null;
     public ?DoublyNode $lastNode = null;
 
-    public function __construct($data = null)
-    {   
-        $newNode = new DoublyNode($data);
-        $this->firstNode = $newNode;
-        $this->lastNode = $newNode;
+    public function __construct(DoublyNode $firstNode = null, DoublyNode $lastNode = null)
+    {
+        $this->firstNode = $firstNode;
+        $this->lastNode = $lastNode;
     }
-    
+
     public function insertAtStart($data)
     {
         $newNode = new DoublyNode($data);
-        $this->firstNode->previousNode = $newNode;
-        $newNode->nextNode = $this->firstNode;
-        $this->firstNode = $newNode;
+
+        if (!$this->firstNode) {
+            $this->firstNode = $newNode;
+            $this->lastNode = $newNode;
+        } else {
+            $this->firstNode->previousNode = $newNode;
+            $newNode->nextNode = $this->firstNode;
+            $this->firstNode = $newNode;
+        }
     }
 
     public function insertAtEnd($data)
     {
         $newNode = new DoublyNode($data);
-        $newNode->previousNode = $this->lastNode;
-        $this->lastNode->nextNode = $newNode;
-        $this->lastNode = $newNode;
+
+        if (!$this->firstNode) {
+            $this->firstNode = $newNode;
+            $this->lastNode = $newNode;
+        } else {
+            $newNode->previousNode = $this->lastNode;
+            $this->lastNode->nextNode = $newNode;
+            $this->lastNode = $newNode;
+        }
     }
 
     public function deleteFromStart(): DoublyNode
