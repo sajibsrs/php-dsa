1f68a2560d848007509a5b0652156601990f57b5
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-25 19:34:38 +0600

    added queue test

---
+++ tests/DataStructures/QueueTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Sajibsrs\DataStructure\Queue
+ */
+class QueueTest extends TestCase
+{
+    public function testAssertTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
