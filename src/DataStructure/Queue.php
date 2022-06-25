7b10efc14f3cbbbde912f8779bf3e03ffc44e222
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-25 19:34:17 +0600

    added queue

--- src/DataStructure/Queue.php
+++ src/DataStructure/Queue.php
@@ -4,7 +4,31 @@ declare(strict_types=1);
 
 namespace Sajibsrs\DataStructure;
 
+use Sajibsrs\DataStructure\LinkedList\Doubly as DoublyLinkedList;
+
 class Queue
 {
-    
+    public $queue;
+    public $data;
+
+    public function __construct()
+    {
+        $this->data = new DoublyLinkedList();
+    }
+
+    public function enqueue($data)
+    {
+        $this->data->insertAtEnd($data);
+    }
+
+    public function dequeue()
+    {
+        $removedNode = $this->data->deleteFromStart();
+        return $removedNode;
+    }
+
+    public function read()
+    {
+        return $this->data->firstNode ?: null;
+    }
 }
