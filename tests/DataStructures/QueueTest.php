7f7ecb05ee43e869fd2e1e00f0e1c00d69230ee0
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-26 05:12:18 +0600

    added queue tests

--- tests/DataStructures/QueueTest.php
+++ tests/DataStructures/QueueTest.php
@@ -1,14 +1,54 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Sajibsrs\DataStructure\Queue;
 
 /**
  * @covers \Sajibsrs\DataStructure\Queue
+ * @covers \Sajibsrs\DataStructure\LinkedList\Doubly
+ * @covers \Sajibsrs\DataStructure\LinkedList\DoublyNode
  */
-class QueueTest extends TestCase
+final class QueueTest extends TestCase
 {
-    public function testAssertTrue(): void
+    private Queue $queue;
+    
+    public function setUp(): void
     {
-        $this->assertTrue(true);
+        $this->queue = new Queue();
+    }
+
+    public function testEnqueue(): void
+    {
+        $this->queue->enqueue('🥝');
+
+        print $this->queue->data->lastNode->data;
+
+        $this->expectOutputString('🥝');
+    }
+
+    public function testDequeue(): void
+    {
+        $this->queue->enqueue('🥝');
+        $this->queue->enqueue('🍑');
+
+        $removedNode = $this->queue->dequeue();
+        
+        print $this->queue->data->lastNode->data;
+        
+        $this->expectOutputString('🍑');
+        $this->assertEquals('🥝', $removedNode->data);
+    }
+
+    public function testRead(): void
+    {
+        $this->queue->enqueue('🥝');
+        $firstNode = $this->queue->read();
+
+        $this->assertSame('🥝', $firstNode->data);
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->queue);
     }
 }
