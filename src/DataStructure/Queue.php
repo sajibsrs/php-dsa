4071045f12fadfd1dba842abba8632f3041e4e1e
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-26 05:16:05 +0600

    added method docs

--- src/DataStructure/Queue.php
+++ src/DataStructure/Queue.php
@@ -15,17 +15,26 @@ class Queue
         $this->data = new DoublyLinkedList();
     }
 
+    /**
+     * Insert data at the end of the queue.
+     */
     public function enqueue($data)
     {
         $this->data->insertAtEnd($data);
     }
 
+    /**
+     * Remove data from to front of the queue.
+     */
     public function dequeue()
     {
         $removedNode = $this->data->deleteFromStart();
         return $removedNode;
     }
     
+    /**
+     * Read data from the front of the queue.
+     */
     public function read()
     {
         return $this->data->firstNode ?: null;
