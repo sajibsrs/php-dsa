6b9cffba272105f3a17f080e2a4748ce389eee15
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-27 03:55:14 +0600

    added binary search tree

---
+++ src/DataStructure/Tree/BinarySearchTree.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sajibsrs\DataStructure\Tree;
+
+use InvalidArgumentException;
+
+class BinarySearchTree
+{
+    public TreeNode $root;
+
+    public function __construct(int $value)
+    {
+        $this->root = new TreeNode($value);
+    }
+    
+    public function insert(int $value, ?TreeNode $node = null)
+    {
+        $currentNode = $node ?: $this->root;
+        
+        if ($value < $currentNode->value) {
+
+            if ($currentNode->left === null) {
+                $currentNode->left = new TreeNode($value);
+            } else {
+                $this->insert($value, $currentNode->left);
+            }
+            
+        } elseif ($value > $currentNode->value) {
+
+            if ($currentNode->right === null) {
+                $currentNode->right = new TreeNode($value);
+            } else {
+                $this->insert($value, $currentNode->right);
+            }
+            
+        } else {
+            throw new InvalidArgumentException("Input value must be less than or greater than it's parent value.", 2001);
+        }
+    }
+}
