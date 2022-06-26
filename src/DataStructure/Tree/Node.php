81ffc997beb71919b07c387b880e60e8fe046819
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-27 02:01:53 +0600

    added tree Node class

---
+++ src/DataStructure/Tree/Node.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sajibsrs\DataStructure\Tree;
+
+class Node
+{
+    public $value = null;
+    public ?Node $left;
+    public ?Node $right;
+
+    public function __construct($value, Node $left = null, Node $right = null)
+    {
+        $this->value = $value;
+        $this->left = $left;
+        $this->right = $right;
+    }
+}
