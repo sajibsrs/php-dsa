1f9c98d1f58cea20f8adc492c9a6cbe9632e542d
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-28 05:39:59 +0600

    search method added

--- src/DataStructure/Tree/BinarySearchTree.php
+++ src/DataStructure/Tree/BinarySearchTree.php
@@ -15,6 +15,27 @@ class BinarySearchTree
         $this->root = $node;
     }
 
+    public function search(int $value, ?TreeNode $node = null)
+    {
+        $currentNode = $node ?: $this->root;
+
+        if ($value === $currentNode->value) {
+            return true;
+        } elseif ($value < $currentNode->value) {
+            if ($currentNode->left !== null) {
+                return $this->search($value, $currentNode->left);
+            } else {
+                return false;
+            }
+        } elseif ($value > $currentNode->value) {
+            if ($currentNode->right !== null) {
+                return $this->search($value, $currentNode->right);
+            } else {
+                return false;
+            }
+        }
+    }
+
     public function insert(int $value, ?TreeNode $node = null)
     {
         $currentNode = $node ?: $this->root;
